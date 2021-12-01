f4c1ddd0142be35c12e2fbbc4bec28daeecf08fc
Ewan McRobert <ewanmcrobert@gmail.com>
2021-12-01 21:05:12 +0000

    tidy up evaluation code so it returns a dictionary rather than loads of separate variables

--- evaluation.py
+++ evaluation.py
@@ -13,17 +13,17 @@ def blur_score(grey_img):
 
 def eval_test_set(model, test_ds, experiment_name):
     
-    cur_best_psnr_score = 0
-    cur_best_ssim_score = 0
-    cur_best_ms_ssim_score = 0
-    cur_best_blur_score = 0
-    cur_brightest_score = 0
-    
-    cur_worst_psnr_score = 0
-    cur_worst_ssim_score = 0
-    cur_worst_ms_ssim_score = 0
-    cur_worst_blur_score = 0
-    cur_darkest_score = 0
+    metrics = {
+        "cur_best_psnr_score": 0,
+        "cur_best_ssim_score": 0,
+        "cur_best_ms_ssim_score": 0,
+        "cur_best_blur_score": 0,
+        "cur_brightest_score": 0,
+        "cur_worst_psnr_score": 0,
+        "cur_worst_ssim_score": 0,
+        "cur_worst_ms_ssim_score": 0,
+        "cur_worst_blur_score": 0,
+        "cur_darkest_score": 0 }
     
     psnr_scores = np.array([])
     ssim_scores = np.array([])
@@ -45,14 +45,14 @@ def eval_test_set(model, test_ds, experiment_name):
         grey_fake = tf.image.rgb_to_grayscale(fake_sen2) # greyscale version of fake image using for calculating image sharpness and brightness
         
         brightness = tf.math.reduce_mean(grey_fake).numpy()
-        if brightness > cur_brightest_score:
-            cur_brightest_score = brightness
+        if brightness > metrics["cur_brightest_score"]:
+            metrics["cur_brightest_score"] = brightness
             cur_brightest_sen1 = sen1
             cur_brightest_sen2 = real_sen2
             cur_brightest_fake = fake_sen2 
         
-        if brightness < cur_darkest_score or cur_darkest_score == 0:
-            cur_darkest_score = brightness
+        if brightness < metrics["cur_darkest_score"] or metrics["cur_darkest_score"] == 0:
+            metrics["cur_darkest_score"] = brightness
             cur_darkest_sen1 = sen1
             cur_darkest_sen2 = real_sen2
             cur_darkest_fake = fake_sen2 
@@ -62,14 +62,14 @@ def eval_test_set(model, test_ds, experiment_name):
         psnr = tf.image.psnr(real_sen2, fake_sen2, max_val=1.0).numpy()
        
         
-        if psnr > cur_best_psnr_score:
-            cur_best_psnr_score = psnr
+        if psnr > metrics["cur_best_psnr_score"]:
+            metrics["cur_best_psnr_score"] = psnr
             cur_best_psnr_sen1 = sen1
             cur_best_psnr_sen2 = real_sen2
             cur_best_psnr_fake = fake_sen2 
         
-        if psnr < cur_worst_psnr_score or cur_worst_psnr_score == 0:
-            cur_worst_psnr_score = psnr
+        if psnr < metrics["cur_worst_psnr_score"] or metrics["cur_worst_psnr_score"] == 0:
+            metrics["cur_worst_psnr_score"] = psnr
             cur_worst_psnr_sen1 = sen1
             cur_worst_psnr_sen2 = real_sen2
             cur_worst_psnr_fake = fake_sen2 
@@ -78,14 +78,14 @@ def eval_test_set(model, test_ds, experiment_name):
         
         blur = blur_score(grey_fake)
         
-        if blur > cur_best_blur_score:
-            cur_best_blur_score = blur
+        if blur > metrics["cur_best_blur_score"]:
+            metrics["cur_best_blur_score"] = blur
             cur_best_blur_sen1 = sen1
             cur_best_blur_sen2 = real_sen2
             cur_best_blur_fake = fake_sen2 
         
-        if blur < cur_worst_psnr_score or cur_worst_psnr_score == 0:
-            cur_worst_blur_score = blur
+        if blur < metrics["cur_worst_psnr_score"] or metrics["cur_worst_psnr_score"] == 0:
+            metrics["cur_worst_blur_score"] = blur
             cur_worst_blur_sen1 = sen1
             cur_worst_blur_sen2 = real_sen2
             cur_worst_blur_fake = fake_sen2 
@@ -94,14 +94,14 @@ def eval_test_set(model, test_ds, experiment_name):
        
         ssim = tf.image.ssim(real_sen2, fake_sen2, max_val=1.0).numpy()
         # get SSIM
-        if ssim > cur_best_ssim_score:
-            cur_best_ssim_score = ssim
+        if ssim > metrics["cur_best_ssim_score"]:
+            metrics["cur_best_ssim_score"] = ssim
             cur_best_ssim_sen1 = sen1
             cur_best_ssim_sen2 = real_sen2
             cur_best_ssim_fake = fake_sen2 
         
-        if ssim < cur_worst_ssim_score or cur_worst_ssim_score == 0:
-            cur_worst_ssim_score = ssim
+        if ssim < metrics["cur_worst_ssim_score"] or metrics["cur_worst_ssim_score"] == 0:
+            metrics["cur_worst_ssim_score"] = ssim
             cur_worst_ssim_sen1 = sen1
             cur_worst_ssim_sen2 = real_sen2
             cur_worst_ssim_fake = fake_sen2 
@@ -110,98 +110,98 @@ def eval_test_set(model, test_ds, experiment_name):
         
         ms_ssim = tf.image.ssim_multiscale(real_sen2, fake_sen2, max_val=1.0).numpy()
         # get SSIM
-        if ssim > cur_best_ms_ssim_score:
-            cur_best_ms_ssim_score = ms_ssim
+        if ssim > metrics["cur_best_ms_ssim_score"]:
+            metrics["cur_best_ms_ssim_score"] = ms_ssim
             cur_best_ms_ssim_sen1 = sen1
             cur_best_ms_ssim_sen2 = real_sen2
             cur_best_ms_ssim_fake = fake_sen2 
         
-        if ssim < cur_worst_ms_ssim_score or cur_worst_ms_ssim_score == 0:
-            cur_worst_ms_ssim_score = ms_ssim
+        if ssim < metrics["cur_worst_ms_ssim_score"] or metrics["cur_worst_ms_ssim_score"] == 0:
+            metrics["cur_worst_ms_ssim_score"] = ms_ssim
             cur_worst_ms_ssim_sen1 = sen1
             cur_worst_ms_ssim_sen2 = real_sen2
             cur_worst_ms_ssim_fake = fake_sen2 
         
         ms_ssim_scores = np.append(ms_ssim_scores, ms_ssim)
     
-    brightness_avg = np.average(brightness_scores)
-    brightness_std = np.std(brightness_scores)  
+    metrics["brightness_avg"] = np.average(brightness_scores)
+    metrics["brightness_std"] = np.std(brightness_scores)  
     
-    tf.print("highest brightness score", cur_brightest_score)
-    tf.print("worst score", cur_darkest_score)
-    tf.print("brightness average", brightness_avg)
-    tf.print("brightness deviation", brightness_std)
+    tf.print("highest brightness score", metrics["cur_brightest_score"])
+    tf.print("worst score", metrics["cur_darkest_score"])
+    tf.print("brightness average", metrics["brightness_avg"])
+    tf.print("brightness deviation", metrics["brightness_std"])
           
     # plot and save best/worst images
     filename = experiment_name + "_brightest_image"
-    plot_images("Brightest Image " + str(cur_brightest_score), cur_brightest_sen1, cur_brightest_sen2, cur_brightest_fake, filename.replace(" ", "_"))
+    plot_images("Brightest Image " + str(metrics["cur_brightest_score"]), cur_brightest_sen1, cur_brightest_sen2, cur_brightest_fake, filename.replace(" ", "_"))
     
     filename = experiment_name + "_darkest_image"
-    plot_images("Darkest Image " + str(cur_darkest_score), cur_darkest_sen1, cur_darkest_sen2, cur_darkest_fake, filename.replace(" ", "_"))
+    plot_images("Darkest Image " + str(metrics["cur_darkest_score"]), cur_darkest_sen1, cur_darkest_sen2, cur_darkest_fake, filename.replace(" ", "_"))
     
     
-    psnr_avg = np.average(psnr_scores)
-    psnr_std = np.std(psnr_scores)  
+    metrics["psnr_avg"] = np.average(psnr_scores)
+    metrics["psnr_std"] = np.std(psnr_scores)  
     
-    tf.print("best score", cur_best_psnr_score)
-    tf.print("worst score", cur_worst_psnr_score)
-    tf.print("psnr average", psnr_avg)
-    tf.print("psnr st deviation", psnr_std)
+    tf.print("best score", metrics["cur_best_psnr_score"])
+    tf.print("worst score", metrics["cur_worst_psnr_score"])
+    tf.print("psnr average", metrics["psnr_avg"])
+    tf.print("psnr st deviation", metrics["psnr_std"])
           
     # plot and save best/worst images
     filename = experiment_name + "_best_psnr"
-    plot_images("Best PSNR score " + str(cur_best_psnr_score), cur_best_psnr_sen1, cur_best_psnr_sen2, cur_best_psnr_fake, filename.replace(" ", "_"))
+    plot_images("Best PSNR score " + str(metrics["cur_best_psnr_score"]), cur_best_psnr_sen1, cur_best_psnr_sen2, cur_best_psnr_fake, filename.replace(" ", "_"))
     
     filename = experiment_name + "_worst_psnr"
-    plot_images("Worst PSNR score " + str(cur_worst_psnr_score), cur_worst_psnr_sen1, cur_worst_psnr_sen2, cur_worst_psnr_fake, filename.replace(" ", "_"))
+    plot_images("Worst PSNR score " + str(metrics["cur_worst_psnr_score"]), cur_worst_psnr_sen1, cur_worst_psnr_sen2, cur_worst_psnr_fake, filename.replace(" ", "_"))
     
-    ssim_avg = np.average(ssim_scores)
-    ssim_std = np.std(ssim_scores)  
+    metrics["ssim_avg"] = np.average(ssim_scores)
+    metrics["ssim_std"] = np.std(ssim_scores)  
     
-    tf.print("best score", cur_best_ssim_score)
-    tf.print("worst score", cur_worst_ssim_score)
-    tf.print("ssim average", ssim_avg)
-    tf.print("ssim st deviation", ssim_std)
+    tf.print("best score", metrics["cur_best_ssim_score"])
+    tf.print("worst score", metrics["cur_worst_ssim_score"])
+    tf.print("ssim average", metrics["ssim_avg"])
+    tf.print("ssim st deviation", metrics["ssim_std"])
           
     # plot and save best/worst images
     filename = experiment_name + "_best_ssim"
-    plot_images("Best SSIM score " + str(cur_best_ssim_score), cur_best_ssim_sen1, cur_best_ssim_sen2, cur_best_ssim_fake, filename.replace(" ", "_"))
+    plot_images("Best SSIM score " + str(metrics["cur_best_ssim_score"]), cur_best_ssim_sen1, cur_best_ssim_sen2, cur_best_ssim_fake, filename.replace(" ", "_"))
     
     filename = experiment_name + "_worst_ssim"
-    plot_images("Worst SSIM score " + str(cur_worst_ssim_score), cur_worst_ssim_sen1, cur_worst_ssim_sen2, cur_worst_ssim_fake, filename.replace(" ", "_"))
+    plot_images("Worst SSIM score " + str(metrics["cur_worst_ssim_score"]), cur_worst_ssim_sen1, cur_worst_ssim_sen2, cur_worst_ssim_fake, filename.replace(" ", "_"))
     
-    ms_ssim_avg = np.average(ms_ssim_scores)
-    ms_ssim_std = np.std(ms_ssim_scores)  
+    metrics["ms_ssim_avg"] = np.average(ms_ssim_scores)
+    metrics["ms_ssim_std"] = np.std(ms_ssim_scores)  
     
-    tf.print("best ms ssim score", cur_best_ms_ssim_score)
-    tf.print("worst score", cur_worst_ms_ssim_score)
-    tf.print("psnr average", ms_ssim_avg)
-    tf.print("psnr st deviation", ms_ssim_std)
+    tf.print("best ms ssim score", metrics["cur_best_ms_ssim_score"])
+    tf.print("worst score", metrics["cur_worst_ms_ssim_score"])
+    tf.print("psnr average", metrics["ms_ssim_avg"])
+    tf.print("psnr st deviation", metrics["ms_ssim_std"])
           
     # plot and save best/worst images
     filename = experiment_name + "_best_ms_ssim"
-    plot_images("Best Multiscale SSIM score " + str(cur_best_ms_ssim_score), cur_best_ms_ssim_sen1, cur_best_ms_ssim_sen2, cur_best_ms_ssim_fake, filename.replace(" ", "_"))
+    plot_images("Best Multiscale SSIM score " + str(metrics["cur_best_ms_ssim_score"]), cur_best_ms_ssim_sen1, cur_best_ms_ssim_sen2, cur_best_ms_ssim_fake, filename.replace(" ", "_"))
     
     filename = experiment_name + "_worst_ms_ssim"
-    plot_images("Worst SSIM score " + str(cur_worst_ms_ssim_score), cur_worst_ms_ssim_sen1, cur_worst_ms_ssim_sen2, cur_worst_ms_ssim_fake, filename.replace(" ", "_"))
+    plot_images("Worst SSIM score " + str(metrics["cur_worst_ms_ssim_score"]), cur_worst_ms_ssim_sen1, cur_worst_ms_ssim_sen2, cur_worst_ms_ssim_fake, filename.replace(" ", "_"))
     
-    blur_avg = np.average(blur_scores)
-    blur_std = np.std(blur_scores)  
+    metrics["blur_avg"] = np.average(blur_scores)
+    metrics["blur_std"] = np.std(blur_scores)  
     
-    tf.print("Best blur scrore", cur_best_blur_score)
-    tf.print("worst blur score", cur_worst_blur_score)
-    tf.print("average blur", blur_avg)
-    tf.print("blur st deviation", blur_std)
+    tf.print("Best blur scrore", metrics["cur_best_blur_score"])
+    tf.print("worst blur score", metrics["cur_worst_blur_score"])
+    tf.print("average blur", metrics["blur_avg"])
+    tf.print("blur st deviation", metrics["blur_std"])
     
      # plot and save best/worst images
     filename = experiment_name + "_best_blur"
-    plot_images("Least blurry image: " + str(cur_best_blur_score), cur_best_blur_sen1, cur_best_blur_sen2, cur_best_blur_fake, filename.replace(" ", "_"))
+    plot_images("Least blurry image: " + str(metrics["cur_best_blur_score"]), cur_best_blur_sen1, cur_best_blur_sen2, cur_best_blur_fake, filename.replace(" ", "_"))
     
     filename = experiment_name + "_worst_ssim"
-    plot_images("Blurriest image: " + str(cur_worst_blur_score), cur_worst_blur_sen1, cur_worst_blur_sen2, cur_worst_blur_fake, filename.replace(" ", "_"))
+    plot_images("Blurriest image: " + str(metrics["cur_worst_blur_score"]), cur_worst_blur_sen1, cur_worst_blur_sen2, cur_worst_blur_fake, filename.replace(" ", "_"))
     # return metrics eg best worst fid,sim,psrn, average, std deviation?
     
-    return brightness_avg, brightness_std, blur_avg, blur_std, cur_best_blur_score, cur_worst_blur_score, cur_brightest_score, cur_darkest_score, psnr_avg, psnr_std, cur_best_psnr_score, cur_worst_psnr_score, ssim_avg, ssim_std, cur_best_ssim_score, cur_worst_ssim_score, ms_ssim_avg, ms_ssim_std, cur_best_ms_ssim_score, cur_worst_ms_ssim_score 
+    return metrics
         
 def plot_images(title, sen1, real_sen2, fake_sen2, filename):
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6))
